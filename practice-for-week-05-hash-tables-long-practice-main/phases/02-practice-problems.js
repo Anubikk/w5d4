20d61827049ae929a952212c94b8f4a3f4ed019d
Hayden Woods <anubikk@gmail.com>
2022-09-29 17:43:25 -0500

    last prob

--- practice-for-week-05-hash-tables-long-practice-main/phases/02-practice-problems.js
+++ practice-for-week-05-hash-tables-long-practice-main/phases/02-practice-problems.js
@@ -12,18 +12,56 @@ function commonElements(arr1, arr2) {
 
 
 function duplicate(arr) {
-  // Your code here
+  const newSet = new Set();
+  for (let i = 0; i < arr.length; i++) {
+    if (newSet.has(arr[i])) {
+      return arr[i];
+    } else {
+      newSet.add(arr[i])
+    }
+  }
 }
 
 
+
+//   let result = arr.filter(nums => {
+
+//   })
+// }
+
+
 function twoSum(nums, target) {
-  // Your code here
+  const newSet = new Set();
+  for (let i = 0; i < arr.length; i++) {
+    if (newSet.has(target - nums[i])) {
+      return true;
+    } else {
+      newSet.add(nums[i])
+    }
+  }
+  return false
 }
 
 
 function wordPattern(pattern, strings) {
-  // Your code here
-}
+  const matches = {}; //create a new key val pair obj by index
+  const wordSet = new Set(); //create a set to hold words
+
+  for (let i = 0; i < pattern.length; i++) {
+    let letter = pattern[i] // letter = A 1
+    if (matches[letter]) { // matches[A] = "dog"
+      if (matches[letter] !== strings[i]) return false; // "dog" = "cat" 8
+      } else { // matches[A] === undefinded 2
+        let word = strings[i] //word = dog 3
+      if (wordSet.has(word)) return false // wordset.has(dog) === true 7
+      wordSet.add(word); //wordSet = {dog, cat} 4
+      matches[letter] = word; //matches = {a: dog, b: cat} 5
+      }
+    }
+    return true; // 6 
+  }
+
+
 
 
 module.exports = [anagrams, commonElements, duplicate, twoSum, wordPattern];
